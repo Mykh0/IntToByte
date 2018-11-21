41871adc3921d5977bd1b777eef29eef334e2906
Unknown <michael_chan@live.nl>
2018-11-21 11:27:22 +0100

    + Done

---
+++ ConvertToByte/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConvertToByte
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int input = Convert.ToInt32(Console.ReadLine());
+
+      Console.WriteLine(Convert.ToString(input, 2).PadLeft(8, '0'));
+    }
+  }
+}
