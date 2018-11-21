b4e77d239ba27fd07e04cd4ffaead77f2fb2f7de
Unknown <michael_chan@live.nl>
2018-11-21 13:17:45 +0100

    * Clamp to 8 bits

--- ConvertToByte/Program.cs
+++ ConvertToByte/Program.cs
@@ -8,7 +8,11 @@ namespace ConvertToByte
     {
       int input = Convert.ToInt32(Console.ReadLine());
 
+      input = input > 255 ? 255 : input;
+      input = input < 0 ? 0 : input;
+
       Console.WriteLine(Convert.ToString(input, 2).PadLeft(8, '0'));
+      Console.ReadLine();
     }
   }
 }
