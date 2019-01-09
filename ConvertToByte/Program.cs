a538e43d1ef29b786033f4b3e85e94f4720d104d
Unknown <michael_chan@live.nl>
2019-01-09 11:10:40 +0100

    Implementation complete

--- ConvertToByte/Program.cs
+++ ConvertToByte/Program.cs
@@ -6,16 +6,9 @@ namespace ConvertToByte
   {
     static void Main(string[] args)
     {
-      double theta = Convert.ToDouble(args[0]);
+      int input = Convert.ToInt32(args[0]);
 
-      double d = 2.466;
-      double c = 343;
-      double fs = 48000;
-
-      double n_samples = (d * fs * Math.Sin(Math.PI * theta / 180)) / c;
-
-      int input = Convert.ToInt32(n_samples);
-
+      input = Convert.ToInt32(Convert.ToDouble(input) / 100 * 255);
       input = input > 255 ? 255 : input < 0 ? 0 : input;
 
       Console.WriteLine(Convert.ToString(input, 2).PadLeft(8, '0'));
