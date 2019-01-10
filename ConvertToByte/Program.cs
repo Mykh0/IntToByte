07c95eef4f263dbe7a8ac8443a6ba56e9d526e16
Unknown <michael_chan@live.nl>
2019-01-10 12:08:14 +0100

    Added ()

--- ConvertToByte/Program.cs
+++ ConvertToByte/Program.cs
@@ -8,7 +8,7 @@ namespace ConvertToByte
     {
       int input = Convert.ToInt32(args[0]);
 
-      input = Convert.ToInt32(Convert.ToDouble(input) / 100 * 255);
+      input = Convert.ToInt32((Convert.ToDouble(input) / 100) * 255);
       input = input > 255 ? 255 : input < 0 ? 0 : input;
 
       Console.WriteLine(Convert.ToString(input, 2).PadLeft(8, '0'));
