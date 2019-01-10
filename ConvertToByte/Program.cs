9e5341c93ef13f0069af62dff2df77d8035d57dc
Unknown <michael_chan@live.nl>
2019-01-10 14:15:08 +0100

    Returns an integer now

--- ConvertToByte/Program.cs
+++ ConvertToByte/Program.cs
@@ -8,10 +8,9 @@ namespace ConvertToByte
     {
       int input = Convert.ToInt32(args[0]);
 
-      input = Convert.ToInt32((Convert.ToDouble(input) / 100) * 255);
       input = input > 255 ? 255 : input < 0 ? 0 : input;
-
-      Console.WriteLine(Convert.ToString(input, 2).PadLeft(8, '0'));
+      input = Convert.ToInt32((Convert.ToDouble(input) / 100) * 255);
+      Console.WriteLine(input);
     }
   }
 }
